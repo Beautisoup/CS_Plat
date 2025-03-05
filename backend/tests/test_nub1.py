d45a8fe1e9b30c7f309e14fccb8139b979b3e20b
aliyun8981839108 <>
2025-03-05 14:13:15 +0800

    Commit test_numb1.py

--- backend/tests/test_nub1.py
+++ backend/tests/test_nub1.py
@@ -1,6 +1,12 @@
-
+import allure
+@allure.feature("nub1")
+@allure.story("nub1")
 def test():
-    assert 1==1
+    with allure.step("step1"):
+        assert 1==1
 
+@allure.feature("nub2")
+@allure.story("nub2")
 def test2():
-    assert 1==1
+    with allure.step("step2"):
+        assert 1==1
