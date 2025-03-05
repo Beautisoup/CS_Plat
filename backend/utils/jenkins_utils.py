2892197367a8ebb2a4b4119311ada30ed05ea573
aliyun8981839108 <>
2025-03-05 13:57:48 +0800

    Commit test_user_service.py

--- backend/utils/jenkins_utils.py
+++ backend/utils/jenkins_utils.py
@@ -12,8 +12,8 @@ class JenkinsUtils:
     # Jenkins 服务对应的token
     PASSWORD = "dd4f889c10164ec39a694432fb1ee583"
     # jenkins要操作的JOB名称
-    JOB = "JenkinsTestLyr1"
-
+    # JOB = "liyuanruiTestPlat"
+    JOB = "liyuanrui_test2"
     @classmethod
     def invoke(cls, invoke_params):
         #invoke_params,接收从测试计划哪来的参数
@@ -27,7 +27,7 @@ class JenkinsUtils:
         job = jenkins_liyuanrui.get_job(cls.JOB)
         # 构建 job， 传入的值必须是字典， key 对应 jenkins 设置的参数名
         # job.invoke(build_params={"task": "CalculatorProject/test/cases/test_div.py"})
-        job.invoke(build_params={"methods": invoke_params})
+        job.invoke(build_params=invoke_params)
         # 获取job 最后一次完成构建的编号
         # http://47.92.149.0:8080/job/ck24/20/allure/
         last_build_number = job.get_last_buildnumber()+1
